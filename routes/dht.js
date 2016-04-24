f6dc7e75794db95cb73396d23aeb51fd0a0e76a7
PMudra <PMudra@users.noreply.github.com>
2016-04-24 17:57:51 +0200

    Simplified configuration file

--- routes/dht.js
+++ routes/dht.js
@@ -5,12 +5,12 @@ var dht = require('dht-sensor');
 var nconf = require('nconf');
 
 nconf.file({file: './config.json'});
-debug("dht:pin=" + nconf.get('dht:pin'));
-debug("dht:sensor=" + nconf.get('dht:sensor'));
+debug("pin=" + nconf.get('pin'));
+debug("sensor=" + nconf.get('sensor'));
 
 router.get('/current', function (req, res, next) {
     try {
-        var current = dht.read(nconf.get('dht:sensor'), nconf.get('dht:pin'));
+        var current = dht.read(nconf.get('sensor'), nconf.get('pin'));
         res.json(current);
     } catch (err) {
         debug(err);
